6f72c86e2f165a79a8aad01ff7cd5dc4b8767b7a
Gustavo Barraza Estrada <barrazagustavo439@gmail.com>
2025-06-16 19:37:39 -0500

    feat the algorit

---
+++ src/JS/msin.js
@@ -0,0 +1,12 @@
+let my_Weigth
+let my_Heigth
+let imc
+
+my_Weigth = prompt(message="please, write your weigth")
+my_Heigth = prompt(message="please, write your heigth")
+
+imc = my_Weigth / (my_Heigth * my_Heigth)
+
+alert("Your IMC is " + imc)
+
+
